ab6c8cda135aa5c9c252b38f6045834be8e7883a
Ryan Parsons <ryanparsons7@gmail.com>
2020-03-09 18:19:41 +0000

    Added all story node text and options

--- adventures/castle/castle_game.js
+++ adventures/castle/castle_game.js
@@ -198,6 +198,409 @@ function setStory(name) {
         text: 'Restart',
         nextid: 1
       }]
+    },
+    {
+      id: 9,
+      text: 'You attempt to run for the cell door, but the guard\'s great perception makes him aware of you as soona as you crawl from under the bed. Your attempt fails and you get subdued by the guard. The guard chains you to the wall for the rest of the night and you are executed in the morning.',
+      options: [{
+        text: 'Restart',
+        nextid: 1
+      }]
+    },
+    {
+      id: 10,
+      text: 'You crawl from under the bed, the guard notices you and immeditely draws his sword upon you.',
+      options: [{
+          text: 'Surrender',
+          nextid: 20
+        },
+        {
+          text: 'Fight (With Glass Shard)',
+          nextid: 12
+        },
+        {
+          text: 'Fight',
+          nextid: 11
+        }
+      ]
+    },
+    {
+      id: 11,
+      text: 'You attempt to fight the guard with your bare fists. You get cut down with the guard\'s sword almost immediately.',
+      options: [{
+        text: 'Restart',
+        nextid: 1
+      }]
+    },
+    {
+      id: 12,
+      text: 'You attempt to fight the guard with the glass shard you found.',
+      options: [{
+          text: 'Slash',
+          nextid: 13
+        },
+        {
+          text: 'Throw',
+          nextid: 14
+        }
+      ]
+    },
+    {
+      id: 13,
+      text: 'You slash at the guard with the glass shard. The glass shard breaks when hitting the guard\'s armour, the guard then strikes you down with his sword',
+      options: [{
+        text: 'Restart',
+        nextid: 13
+      }]
+    },
+    {
+      id: 14,
+      text: 'You throw the glass shard at the guard, it lands directly into the guard\'s eye, killing him.',
+      options: [{
+        text: 'Leave the cell',
+        nextid: 16
+      }]
+    },
+    {
+      id: 16,
+      text: 'You\'re in a hallway, to the west you see a door and to the east, you see another door.',
+      options: [{
+          text: 'Go West',
+          nextid: 17
+        },
+        {
+          text: 'Go East',
+          nextid: 21
+        }
+      ]
+    },
+    {
+      id: 17,
+      text: 'You are at the end of the hall, there is a hardwood door to your direct west and the hallway continues to the east.',
+      options: [{
+          text: 'Open the door with the guard key',
+          nextid: 19
+        },
+        {
+          text: 'Open the door',
+          nextid: 18
+        },
+        {
+          text: 'Go East',
+          nextid: 16
+        }
+      ]
+    },
+    {
+      id: 18,
+      text: 'You try to open the door, but it is locked.',
+      options: [{
+        text: 'Come away from the door',
+        nextid: 17
+      }]
+    },
+    {
+      id: 19,
+      text: 'You are at the end of a hallway, to your direct east, is a door. In the distance to the west, you see a light source.',
+      options: [{
+          text: 'Head West',
+          nextid: 30
+        },
+        {
+          text: 'Open the door',
+          nextid: 17
+        }
+      ]
+    },
+    {
+      id: 20,
+      text: 'You surrender to the guard. The guard chains you to the wall for the rest of the night and you are executed in the morning',
+      options: [{
+        text: 'Restart',
+        nextid: 1
+      }]
+    },
+    {
+      id: 21,
+      text: 'You reach a 3 way junction of hallways, there is a hallway going North, South and West.',
+      options: [{
+          text: 'Go West',
+          nextid: 16
+        },
+        {
+          text: 'Go South',
+          nextid: 22
+        },
+        {
+          text: 'Go North',
+          nextid: 26
+        }
+      ]
+    },
+    {
+      id: 22,
+      text: 'You are at a door at the end of a hallway. The hallway leads North.',
+      options: [{
+          text: 'Go North',
+          nextid: 21
+        },
+        {
+          text: 'Open the door with the key',
+          nextid: 24
+        },
+        {
+          text: 'Open the door',
+          nextid: 23
+        }
+      ]
+    },
+    {
+      id: 23,
+      text: 'You try to open the door, but it is locked.',
+      options: [{
+        text: 'Come away from the door',
+        nextid: 22
+      }]
+    },
+    {
+      id: 24,
+      text: 'You unlock the door with the dungeon key. You open the door and see the outside world.',
+      options: [{
+        text: 'Run away',
+        nextid: 25
+      }]
+    },
+    {
+      id: 25,
+      text: 'You successfully escape the castle grounds and live happily ever after.',
+      options: [{
+        text: 'Restart',
+        nextid: 1
+      }]
+    },
+    {
+      id: 26,
+      text: 'You are at a door at the end of a hallway, the hallway leads south',
+      options: [{
+          text: 'Go South',
+          nextid: 21
+        },
+        {
+          text: 'Open the door and leave the room',
+          nextid: 27
+        }
+      ]
+    },
+    {
+      id: 27,
+      text: 'You are inside what looks like guard sleeping quarters. There are multiple beds, a door and a wooden box.',
+      options: [{
+          text: 'Check under the beds',
+          nextid: 28
+        },
+        {
+          text: 'Check inside the wooden box',
+          nextid: 29
+        },
+        {
+          text: 'Go through the door',
+          nextid: 268531
+        }
+      ]
+    },
+    {
+      id: 28,
+      text: 'You look under every bed in the room, you find nothing but dust and cobwebs.',
+      options: [{
+        text: 'Come away from the beds',
+        nextid: 27
+      }]
+    },
+    {
+      id: 29,
+      text: 'You open the wooden box to find a Steel Sword',
+      options: [{
+        text: 'Take Steel Sword and close the box',
+        nextid: 27
+      }]
+    },
+    {
+      id: 30,
+      text: 'You are at a corner of a hallway, in the corner on the wall is a torch holder containin lit torches. The hallway leads North into the distance and to the east to a door.',
+      options: [{
+          text: 'Go East',
+          nextid: 19
+        },
+        {
+          text: 'Take Torch',
+          nextid: 31
+        },
+        {
+          text: 'Head North',
+          nextid: 32
+        }
+      ]
+    },
+    {
+      id: 31,
+      text: 'You take one of the torches off the wall.',
+      options: [{
+        text: 'Come away from the wall',
+        nextid: 30
+      }]
+    },
+    {
+      id: 32,
+      text: 'You are at a T-Junction of hallways. The hallways lead West, South and East. To the east, there is a no light sources to be seen,',
+      options: [{
+          text: 'Go South',
+          nextid: 30
+        },
+        {
+          text: 'Go East',
+          nextid: 33
+        },
+        {
+          text: 'Go East with the lit torch',
+          nextid: 36
+        },
+        {
+          text: 'Go West',
+          nextid: 34
+        }
+      ]
+    },
+    {
+      id: 33,
+      text: 'You venture in into the dark passage with no light source. You suddenly get jumped by an unknown monster. You quickly die.',
+      options: [{
+        text: 'Restart',
+        nextid: 1
+      }]
+    },
+    {
+      id: 34,
+      text: 'You are inside what looks like a storage room. There are multiple boxes scattered around the room.',
+      options: [{
+          text: 'Leave the room',
+          nextid: 32
+        },
+        {
+          text: 'Open the boxes',
+          nextid: 35
+        }
+      ]
+    },
+    {
+      id: 35,
+      text: 'You open the various boxes in the room and come across a potion. It reads on the label: "Light-Foot Potion"',
+      options: [{
+        text: 'Take the potion',
+        nextid: 34
+      }]
+    },
+    {
+      id: 36,
+      text: 'You are inside a dark hallway with your trusty torch as a light source. The hallway leads West and North.',
+      options: [{
+          text: 'Go North',
+          nextid: 41
+        },
+        {
+          text: 'Go West',
+          nextid: 32
+        }
+      ]
+    },
+    {
+      id: 37,
+      text: 'You approach the shiny object. Due to your noisey movements, the dragon awakens and you get killed in a burst of fire.',
+      options: [{
+        text: 'Restart',
+        nextid: 1
+      }]
+    },
+    {
+      id: 38,
+      text: 'To make your movements quiet, you take the potion and move quietly to the shiny object on the pile of Skeletons. You notice the shiny object is a key, possibly for the entire castle.',
+      options: [{
+        text: 'Pick up the key',
+        nextid: 43
+      }]
+    },
+    {
+      id: 39,
+      text: 'You attack the dragon with your bear fists and get incinerated in a gust of fire.',
+      options: [{
+        text: 'Restart',
+        nextid: 1
+      }]
+    },
+    {
+      id: 40,
+      text: 'You attack the dragon with your sword, with a few quick strikes at it\'s head, the dragon dies.',
+      options: [{
+        text: 'Step back from the dead dragon',
+        nextid: 42
+      }]
+    },
+    {
+      id: 41,
+      text: 'You are inside a massive cavern, in the center is a sleeping dragon. Surrounding the room is piles of Skeletons, one of the piles seem to have a shiny item upon it.',
+      options: [{
+          text: 'Attack the dragon with your bare fists',
+          nextid: 39
+        },
+        {
+          text: 'Attack the dragon your sword',
+          nextid: 40
+        },
+        {
+          text: 'Investigate the shiny item',
+          nextid: 37
+        },
+        {
+          text: 'Take the potion and investigate the shiny item',
+          nextid: 38
+        }
+      ]
+    },
+    {
+      id: 42,
+      text: 'You are inside a massive cavern, in the center is a dead dragon. Surrounding the room is piles of Skeletons, one of the piles seem to have a shiny item upon it.',
+      options: [{
+          text: 'Leave the cavern',
+          nextid: 36
+        },
+        {
+          text: 'Investigate the shiny item',
+          nextid: 45
+        }
+      ]
+    },
+    {
+      id: 43,
+      text: 'You collect the key',
+      options: [{
+        text: 'Go back to the center of the cavern',
+        nextid: 41
+      }]
+    },
+    {
+      id: 44,
+      text: 'You collect the key',
+      options: [{
+        text: 'Go back to the center of the cavern',
+        nextid: 42
+      }]
+    },
+    {
+      id: 45,
+      text: 'You move to the shiny object on the pile of Skeletons. You notice the shiny object is a key, possibly for the entire castle.',
+      options: [{
+        text: 'Collect the key',
+        nextid: 44
+      }]
     }
   ]
 }
